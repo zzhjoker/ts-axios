e1947b83c7f56424af65bcda879ff9929f0dfc5d
zouzenghu <17600136957@163.com>
2021-05-16 17:37:41 +0800

    feat(创建入口文件): 定义axios 入口文件，定义入口函数，定义入口函数参数接口

--- src/ts-axios.ts
+++ src/ts-axios.ts
@@ -1,6 +1,7 @@
-// Import here Polyfills if needed. Recommended core-js (npm i -D core-js)
-  // import "core-js/fn/array.find"
-  // ...
-export default class DummyClass {
+import { AxiosRequestConfig } from "./types/index";
+
+function axios(config: AxiosRequestConfig) {
 
 }
+
+export default axios
